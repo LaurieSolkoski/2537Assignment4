e4ed932b4e0546e41c0f55c5b19646a2b6c8456e
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2023-05-23 23:07:19 -0700

    added powerup and difficulty levels

--- index.js
+++ index.js
@@ -3,61 +3,78 @@ let secondCard = undefined;
 let clicks = 0;
 let pokemonData = [];
 let matches = 0;
-let remaining = 0; // Initialized later after pokemonData is fully populated
+let remaining = 0;
+let timerInterval;
 
-const updateTime = () => {
-    time++;
-    let countdown = 20 - time;
-    $("#timer").text(time);
-    $("#countdown").text(countdown); //display countdown
-    if (countdown <= 0) { // Stop the game when countdown reaches 0
-        clearInterval(timerInterval);
-        alert("Time's up! Game over.");
-    }
+const difficultyLevels = {
+    "easy": { numPairs: 3, timeLimit: 20 },
+    "medium": { numPairs: 6, timeLimit: 60 },
+    "hard": { numPairs: 12, timeLimit: 100 },
 };
+let difficulty = "easy"; // Default difficulty
 
-
-
-// Fetch Pokémon data
-const getPokemonData = async () => {
-    time = 0; // Reset the time
-    $("#s").text(time); // Initially set time to 0
-    $("#countdown").text(20); // Initially set countdown to 20
-    timerInterval = setInterval(updateTime, 1000); // Update the time every second
-    try {
-        pokemonData = [];
-        for (let i = 0; i < 3; i++) {
-            const randomId = Math.floor(Math.random() * 151) + 1; // Generate a random ID between 1 and 151 (for Gen 1 Pokémon)
-            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
-            const pokeData = await res.json();
-            const pokemon = {
-                name: pokeData.name,
-                url: pokeData.url,
-                imageFront: pokeData.sprites.other["official-artwork"].front_default,
-                imageBack: pokeData.sprites.other["official-artwork"].front_default
-            };
-            pokemonData.push(pokemon);
+const updateTime = (timeLimit) => {
+    let time = 0;
+    timerInterval = setInterval(() => {
+        time++;
+        let countdown = timeLimit - time;
+        $("#timer").text(time);
+        $("#countdown").text(countdown);
+        if (countdown <= 0) {
+            clearInterval(timerInterval);
+            alert("Time's up! Game over.");
         }
-        // Duplicating the pokemon data to create pairs
-        pokemonData = [...pokemonData, ...pokemonData];
-        // Shuffling the array to randomize the card positions
-        pokemonData.sort(() => Math.random() - 0.5);
-
-        // Initialize remaining matches after Pokemon data has been fetched and duplicated
-        remaining = pokemonData.length / 2;
-        $('#left').text(remaining);
-
-        renderCards();
-    } catch (error) {
-        console.error('Error:', error);
-    }
+    }, 1000);
 };
 
-// Render cards
+const getPokemonData = async (numPairs) => {
+    time = 0;
+    updateTime(difficultyLevels[difficulty].timeLimit);
+    pokemonData = [];
+    for (let i = 0; i < numPairs; i++) {
+        const randomId = Math.floor(Math.random() * 151) + 1;
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+        const pokeData = await res.json();
+        const pokemon = {
+            name: pokeData.name,
+            url: pokeData.url,
+            imageFront: pokeData.sprites.other["official-artwork"].front_default,
+            imageBack: pokeData.sprites.other["official-artwork"].front_default
+        };
+        pokemonData.push(pokemon);
+    }
+    pokemonData = [...pokemonData, ...pokemonData];
+    pokemonData.sort(() => Math.random() - 0.5);
+
+    remaining = pokemonData.length / 2;
+    $('#left').text(remaining);
+    $('#total').text(remaining); // Update total pairs
+
+    renderCards();
+};
+
 const renderCards = () => {
     const gameGrid = $('#game_grid');
+    gameGrid.empty(); // Clear the game grid before rendering new cards
+
+    let numPairs = difficultyLevels[difficulty].numPairs;
+    let cardWidth = 100 / numPairs; // calculate the width of the card depending on the number of pairs
+
+    gameGrid.css('flex-direction', 'row');
+    gameGrid.css('flex-wrap', 'wrap');
+
+    if (numPairs > 3) { // If the game is in medium or hard mode
+        gameGrid.css('width', '800px'); // Increase the width of the game grid
+        gameGrid.css('flex-direction', 'row');
+        gameGrid.css('flex-wrap', 'wrap');
+    } else {
+        gameGrid.css('width', '600px'); // Default width for easy mode
+    }
+
     pokemonData.forEach((pokemon, index) => {
         const card = $('<div>').addClass('card');
+        card.css('width', `${cardWidth}%`); // Set the card width dynamically
+
         const frontFace = $('<img>').addClass('front_face').attr('src', pokemon.imageFront).attr('id', `card${index}`);
         const backFace = $('<img>').addClass('back_face').attr('src', 'back.webp');
         card.append(backFace, frontFace);
@@ -65,6 +82,8 @@ const renderCards = () => {
     });
     setup();
 };
+
+
 
 const setup = () => {
     $("#dark").on("click", function () {
@@ -120,4 +139,42 @@ const setup = () => {
     });
 };
 
-$(document).ready(getPokemonData);
+
+
+const showPowerUp = () => {
+    $(".card:not(.matched)").addClass("flip");
+  
+    setTimeout(() => {
+      $(".card:not(.matched)").removeClass("flip");
+    }, 2000);
+  };
+  
+  const powerUpRandomizer = () => {
+    const powerUpIntervals = {
+      "easy": 10000,   // 10 seconds
+      "medium": 20000, // 20 seconds
+      "hard": 30000    // 30 seconds
+    };
+  
+    setTimeout(() => {
+      alert("Power up activated: Revealing all cards for 2 seconds!");
+      showPowerUp();
+      powerUpRandomizer(); // Recurse to keep generating power-ups
+    }, powerUpIntervals[difficulty]);
+  };
+
+
+
+$(document).ready(() => {
+    // Add event listeners for difficulty selection
+    $("#difficulty input[type='radio']").change((event) => {
+      difficulty = event.target.value;
+    });
+  
+    $("#start").click(() => {
+      const { numPairs } = difficultyLevels[difficulty];
+      getPokemonData(numPairs);
+      powerUpRandomizer(); // Initialize power-up randomizer
+    });
+  });
+  
